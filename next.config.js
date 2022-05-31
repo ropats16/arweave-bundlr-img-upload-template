0f7e72e0ceadc796a73b1a5cb7c64beead4c78c7
ropats16 <rohitcpathare@gmail.com>
2022-06-01 00:08:37 +0530

    Added loader and next config for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+    }
+  },
+  images: {
+    loader: "custom"
+  },
 }
 
 module.exports = nextConfig
